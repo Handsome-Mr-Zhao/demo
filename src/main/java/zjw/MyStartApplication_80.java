21eff1514e48bbf8e9913d94c69ac65705da5bf7
1920139555@qq.com <1920139555@qq.com>
2020-03-25 14:26:10 +0800

    diyicitijiao

---
+++ src/main/java/zjw/MyStartApplication_80.java
@@ -0,0 +1,17 @@
+package zjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Desc 实体类
+ * @Author zjw
+ * @Date 2020-03-25-14:22
+ */
+@SpringBootApplication
+public class MyStartApplication_80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStartApplication_80.class);
+    }
+}
---
+++ src/main/java/zjw/web/MyController.java
@@ -0,0 +1,20 @@
+package zjw.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Desc 实体类
+ * @Author zjw
+ * @Date 2020-03-25-14:23
+ */
+@RestController
+@RequestMapping("/zjw/")
+public class MyController {
+
+    @GetMapping("getName")
+    public String getName() {
+        return "赵家伟";
+    }
+}
